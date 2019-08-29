5e83e983a60b052b919a252539f6417e566a22a9
Raymundo Robles J <ray@MacBook-Robles.local>
2019-08-28 23:20:31 -0500

    Listo front de registro

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, NgForm, Validators } from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 
 @Component({
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validator, Validators } from "@angular/forms";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -8,16 +10,22 @@ import { FormGroup, FormControl, Validator, Validators } from "@angular/forms";
 })
 export class RegisterComponent implements OnInit {
 
-  frmRegistro: FormGroup;
+  frmUsuario: FormGroup;
+  hide = true;
+  rememberme: boolean = false;
 
-  constructor() { }
+  constructor( private router: Router ) { }
 
   ngOnInit() {
-    this.frmRegistro = new FormGroup({
-      'medico' : new FormControl('',[Validators.required]),
-      'nombre': new FormControl('',[Validators.required, Validators.minLength(2)]),
-      'apellido': new FormControl('',)
+    this.frmUsuario = new FormGroup({
+      'emailControl' : new FormControl('',[Validators.required,
+                                    Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
+      'passControl': new FormControl('',[Validators.required, Validators.minLength(6)]),
+      'nombreControl': new FormControl('',[Validators.required, Validators.minLength(2)])
     });
   }
 
+  registraNuevo() {
+    console.log(this.frmUsuario);
+  }
 }
