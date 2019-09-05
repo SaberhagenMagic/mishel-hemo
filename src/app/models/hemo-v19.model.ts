cf331d537283de37d58cc34d7affc313dae0aa0d
acalderong <acalderong@correo.cjf.gob.mx>
2019-09-05 10:07:06 -0500

    modelo de hemofilia

---
+++ src/app/models/hemo-v19.model.ts
@@ -0,0 +1,26 @@
+export class hemo {
+    medico: string;
+    rfc: string;
+    beneficiario: number;
+    apaterno: string;
+    amaterno: string;
+    nombre: string;
+    fechanacimiento: Date;
+    talla: number;
+    peso: number;
+    fechadiagnostico: Date;
+    tipohemofilia: string;
+    factordeficiente: string;
+    ultimamedicion_fd: number;
+    fechaultimamedicion_fd: Date;
+    articulacionblanco: string;
+    tipohemofiliaseveridad: string;
+    respuestadesmopresina: boolean;
+    presentainhibidores: boolean;
+    ultimamedicióninhibidores: number;
+    fechaultimamedicióninhibidores: Date;
+    tratamientoprofilaxisdemanda: string;
+    descripciónmedicamento: string;
+    ui_kg: number;
+    frecuenciasemanal: number;
+}
