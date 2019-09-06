b519d1c63304b96c2509a6105aac2a2d65e1bc2d
acalderong <acalderong@correo.cjf.gob.mx>
2019-09-06 14:25:47 -0500

    ligando formulario hemo

--- src/app/hemofilia-v19/hemofilia-v19.component.ts
+++ src/app/hemofilia-v19/hemofilia-v19.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { hemo } from "../models/hemo-v19.model";
 
 @Component({
   selector: 'app-hemofilia-v19',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HemofiliaV19Component implements OnInit {
 
-  constructor() { }
+  patient = new hemo();
+  frmPatient: FormGroup;
+
+  constructor() {
+    this.formPatienInit();
+  }
 
   ngOnInit() {
   }
 
+  formPatienInit(){
+    this.frmPatient = new FormGroup({
+      'registroId' : new FormControl(),
+      'medico': new FormControl(),
+      'rfc': new FormControl(),
+      'beneficiario': new FormControl(),
+      'apaterno': new FormControl(),
+      'amaterno': new FormControl(),
+      'nombre': new FormControl(),
+      'fechanacimiento': new FormControl(),
+      'talla': new FormControl(),
+      'peso': new FormControl(),
+      'fechadiagnostico': new FormControl(),
+      'tipohemofilia': new FormControl(),
+      'factordeficiente': new FormControl(),
+      'ultimamedicion_fd': new FormControl(),
+      'fechaultimamedicion_fd': new FormControl(),
+      'articulacionblanco': new FormControl(),
+      'tipohemofiliaseveridad': new FormControl(),
+      'respuestadesmopresina': new FormControl(),
+      'presentainhibidores': new FormControl(),
+      'ultimamedicióninhibidores': new FormControl(),
+      'fechaultimamedicióninhibidores': new FormControl(),
+      'tratamientoprofilaxisdemanda': new FormControl(),
+      'descripciónmedicamento': new FormControl(),
+      'ui_kg': new FormControl(),
+      'frecuenciasemanal': new FormControl()
+    });
+  }
+
+  saveRegister() {
+    console.log(this.frmPatient);
+  }
 }
--- src/app/models/hemo-v19.model.ts
+++ src/app/models/hemo-v19.model.ts
@@ -1,4 +1,5 @@
 export class hemo {
+    registroId: string;
     medico: string;
     rfc: string;
     beneficiario: number;
